94b85e31e6de9a90ef015075bd2249531212263d
Sean Burke <seanburke012@gmail.com>
2025-01-08 19:59:27 -0800

    python 201 labs

---
+++ python-201-main/projects/barking-shibes.py
@@ -0,0 +1,9 @@
+# Use a quotes API, e.g. https://zenquotes.io/api/random
+# to fetch a random quote. Then use string manipulation to convert it to
+# Doge speech (https://en.wikipedia.org/wiki/Doge_(meme))
+# Create a tiny webpage that displays a doge image together
+# with the altered quote. You can get an image URL from another API:
+# https://dog.ceo/api/breed/shiba/images/random
+# Write the code logic to make the API calls and assign the output to
+# `doged_quote` and `doge_image_url` respectively.
+# Then write the `html` string to a `.html` file and look at it in your browser.
