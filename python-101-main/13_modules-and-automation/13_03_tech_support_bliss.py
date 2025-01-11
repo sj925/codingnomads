6ec5633f9e2a6c61b85bdf767bb0290d71aebce5
Sean Burke <seanburke012@gmail.com>
2025-01-10 17:20:35 -0800

    meant section 13 labs.

--- python-101-main/13_modules-and-automation/13_03_tech_support_bliss.py
+++ python-101-main/13_modules-and-automation/13_03_tech_support_bliss.py
@@ -1,9 +1,9 @@
 # Use the built-in `platform` module to print out the following info:
 import platform
 
-print(f"{'Platform:':>10} {platform.platform()}")  # Shows your OS platform
-print(f"{'Compiler:':>10} {platform.python_compiler()}")  # Shows Python compiler
-print(f"{'Python:':>10} {platform.python_version()}")  # Shows Python version
-print(f"{'System:':>10} {platform.system()}")  # Shows OS name
-print(f"{'Release:':>10} {platform.release()}")  # Shows OS release
-print(f"{'System:':>10} {platform.processor()}")  # Shows processor type
+print(f"{'Platform:':>10} {platform.platform()}")
+print(f"{'Compiler:':>10} {platform.python_compiler()}")
+print(f"{'Python:':>10} {platform.python_version()}")
+print(f"{'System:':>10} {platform.system()}")
+print(f"{'Release:':>10} {platform.release()}")
+print(f"{'System:':>10} {platform.processor()}")
