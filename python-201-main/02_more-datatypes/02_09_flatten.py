c3193736af55ca776ff9ac09cbb25dc75c1ddc73
Sean Burke <seanburke012@gmail.com>
2025-01-13 17:34:50 -0800

    02_09 solution

--- python-201-main/02_more-datatypes/02_09_flatten.py
+++ python-201-main/02_more-datatypes/02_09_flatten.py
@@ -10,3 +10,19 @@
 # to flatten a list of any depth. Can you understand the code used?
 
 starter_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+flat_list = []
+for row in starter_list:
+    flat_list.extend(row)
+print(flat_list)
+    
+#deeply nested lists
+#note this will only work on types of list. not lists of dicts
+
+def flatten_deep(lst):
+    flat_list = []
+    for item in lst:
+        if isinstance(item, list):
+            flat_list.extend(flatten_deep(item))
+        else:
+            flat_list.append(item)
+    return flat_list
