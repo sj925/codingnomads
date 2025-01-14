d8380367106530eee544ba507522108c4987d80c
Sean Burke <seanburke012@gmail.com>
2025-01-13 20:47:38 -0800

    02_11 and 02_12 solutions

--- python-201-main/02_more-datatypes/02_12_add_or_not.py
+++ python-201-main/02_more-datatypes/02_12_add_or_not.py
@@ -6,3 +6,29 @@
 # their input is a duplicate and deduct a point.
 # If the user loses 5 points, quit the program.
 # They win if they manage to create a set that has more than 10 items.
+
+health = 5
+myset = set()
+while health > 0:
+    userinput = input("enter a number: ")
+    if userinput.isdigit():
+        if userinput in myset:
+            print("this number is already in your set! that'll cost you 1 health")
+            health -= 1
+            if health == 0:
+                print("sorry you ran out of health!")
+        else:
+            myset.add(userinput)
+        if len(myset) > 10:
+            print(f"you won the game as your set is greater than 10 numbers: {myset}")
+            break
+    else:
+        print("you did not enter a number")
+        break
+   
+    
+
+
+    
+    
+    
