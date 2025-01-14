15056e09c918f9cd354f1a2e7f699f308c5cb58f
Sean Burke <seanburke012@gmail.com>
2025-01-13 17:03:59 -0800

    02_08 solution:

--- python-201-main/02_more-datatypes/02_08_remove_duplicates.py
+++ python-201-main/02_more-datatypes/02_08_remove_duplicates.py
@@ -4,3 +4,23 @@
 # 2. Use a loop and a second list to solve it more manually
 
 list_ = [1, 2, 3, 4, 3, 4, 5]
+
+#first way 
+
+list_ = list(set(list_))
+print(list_)
+
+#second way if first list above was not already modified to catch dupes. would need to comment out first approach for second to work
+        
+list2 = []
+
+for num in list_:
+    if num not in list2:
+        list2.append(num)
+    else: 
+        print("num already in list!")
+print(list2)
+
+
+ 
+    
