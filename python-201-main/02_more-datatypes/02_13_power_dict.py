0fc424bf91633a9c4adb061fcb2673143a864520
Sean Burke <seanburke012@gmail.com>
2025-01-14 20:16:52 -0800

    02_13 solution

--- python-201-main/02_more-datatypes/02_13_power_dict.py
+++ python-201-main/02_more-datatypes/02_13_power_dict.py
@@ -1,3 +1,7 @@
 # Write a script that creates a dictionary of keys, `n`
 # and values `n * n` for numbers 1 to 10. For example:
 # result = {1: 1, 2: 4, 3: 9, ... and so on}
+
+mydict = {x : x ** 2 for x in range(1, 11)}
+
+print(mydict)
