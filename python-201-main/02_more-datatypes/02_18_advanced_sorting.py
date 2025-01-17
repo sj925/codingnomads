c9c062d5a6895afb66ffed0f8406837ecb658809
Sean Burke <seanburke012@gmail.com>
2025-01-16 16:39:58 -0800

    02_18 solution

--- python-201-main/02_more-datatypes/02_18_advanced_sorting.py
+++ python-201-main/02_more-datatypes/02_18_advanced_sorting.py
@@ -8,3 +8,13 @@
 # even if you don't yet completely understand why it works the way it does:
 # https://docs.python.org/3/howto/sorting.html#key-functions
 # Feel free to discuss any questions you have with your mentor and on the forum!
+
+input_dict = {"item1": 5, "item2": 6, "item3": 1}
+my_lst = []
+
+for k, v in input_dict.items():
+    tupes = (k, v)
+    my_lst.append(tupes)
+
+sorted_lst = sorted(my_lst, key=lambda x: x[1])
+print(sorted_lst)
