63ffc8b3666af2db9cb473b1f61f4437488495e0
Sean Burke <seanburke012@gmail.com>
2025-01-16 20:10:28 -0800

    03_02 solution

--- python-201-main/03_file-input-output/03_02_words_analysis.py
+++ python-201-main/03_file-input-output/03_02_words_analysis.py
@@ -4,3 +4,22 @@
 # 1. The shortest word (if there is a tie, print all)
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
+
+from pathlib import Path
+
+words = Path.cwd().joinpath("words.txt")
+lst = []
+
+with open(words, "r") as file:
+    for word in file:
+        lst.append(word.strip())
+
+min_len = (len(min(lst, key=len)))
+max_len = (len(max(lst, key=len)))
+min_words = [ word for word in lst if len(word) == min_len]
+max_words = [word for word in lst if len(word) == max_len]
+print(len(lst))
+print(min_words, max_words)
+        
+      
+    
