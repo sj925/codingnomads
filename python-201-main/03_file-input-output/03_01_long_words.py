4ea44be59fb314228ca54ff200206434adce8a67
Sean Burke <seanburke012@gmail.com>
2025-01-16 19:48:11 -0800

    03_01 solution

--- python-201-main/03_file-input-output/03_01_long_words.py
+++ python-201-main/03_file-input-output/03_01_long_words.py
@@ -1,2 +1,12 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+
+from pathlib import Path
+
+words = Path.cwd().joinpath("words.txt")
+
+with open(words, "r") as file:
+    for word in file:
+        if len(word.strip()) > 20:
+            print(word.strip()) 
+    
