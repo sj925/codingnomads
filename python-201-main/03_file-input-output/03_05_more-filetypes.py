c294b9a5e4676cbe8b4baf71ea6e735b8f666a89
Sean Burke <seanburke012@gmail.com>
2025-01-20 14:15:12 -0800

    03_05 solve

--- python-201-main/03_file-input-output/03_05_more-filetypes.py
+++ python-201-main/03_file-input-output/03_05_more-filetypes.py
@@ -2,3 +2,30 @@
 # in your CSV file. Remember that the format of your output needs to be
 # consistent across multiple runs of your script. This means you'll need to
 # make a compromise and choose which file types you want to record beforehand.
+from pathlib import Path
+import csv
+
+script_dir = Path(__file__).parent
+
+scan_dir = script_dir
+csv_file = script_dir / "file-counter/filecounts.csv"
+file_types_to_track = [".py", ".txt", ".csv", ".jpg", ".png", ".md"]
+file_counts = {file_type: 0 for file_type in file_types_to_track}
+
+
+for file in scan_dir.glob("**/*"):
+    if file.is_file() and file.suffix in file_types_to_track:
+        file_counts[file.suffix] += 1
+
+output_data = [[file_type, count] for file_type, count in file_counts.items()]
+
+with csv_file.open("w", newline="") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(["File Type", "Count"])
+    writer.writerows(output_data)
+
+print("\nUpdated CSV contents:")
+with csv_file.open("r") as infile:
+    reader = csv.reader(infile)
+    for row in reader:
+        print(row)
