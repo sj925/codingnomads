79934bbdcdb336c27b6f35efd146e5cfae479b95
Sean Burke <seanburke012@gmail.com>
2025-01-20 13:48:49 -0800

    03_04 solve

--- python-201-main/03_file-input-output/03_04_pathlib_reader.py
+++ python-201-main/03_file-input-output/03_04_pathlib_reader.py
@@ -1,3 +1,27 @@
 # Refactor your file counter script to use `pathlib` also for
 # reading and writing to your CSV file. Make sure to handle the
 # path in a way so that you can run the script from anywhere.
+
+from pathlib import Path
+import csv
+
+script_dir = Path(__file__).parent
+csv_file = script_dir / "file-counter/filecounts.csv"
+
+if csv_file.exists():
+    with open (csv_file, "r") as infile:
+        reader = csv.reader(infile)
+        for row in reader: 
+            print(row)
+else:
+    print("the csv file you are trying to open does not exist")
+    
+data_to_write = {'': 8, '.csv': 2, '.md': 2, '.png': 11}
+
+with open (csv_file, "a") as outfile: 
+    writer = csv.writer(outfile)
+    writer.writerows(list(data_to_write.items()))
+    print("data written successfully")
+    
+
+
