bbc92cacce9574b1054002298cb1f33845f0a5f4
Sean Burke <seanburke012@gmail.com>
2025-01-20 13:27:07 -0800

    csv stuff ?

--- python-201-main/03_file-input-output/file-counter/analyze.py
+++ python-201-main/03_file-input-output/file-counter/analyze.py
@@ -1,1 +1,8 @@
 # Use the `csv` module to read in and count the different file types.
+import csv
+
+with open("filecounts.csv", "r") as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames=["Folder", "CSV", "MD", "PNG"])
+    counts = list(reader)
+
+print(counts)
--- python-201-main/03_file-input-output/file-counter/csvwrite.py
+++ python-201-main/03_file-input-output/file-counter/csvwrite.py
@@ -1,1 +1,10 @@
 # Write the file counts to a `.csv` file.
+import csv
+counts = {'': 8, '.csv': 2, '.md': 2, '.png': 11}
+
+with open ("filecounts.csv", "w") as file:
+    writer = csv.DictWriter(file, fieldnames=["folder", "csv", ".md", ".png"])
+    writer.writeheader()
+    writer.writerows
+
+
