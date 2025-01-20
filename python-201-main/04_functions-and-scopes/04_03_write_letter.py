8c3fbb1b646fc6c4dade683ff67b0fbabe4cd09d
Sean Burke <seanburke012@gmail.com>
2025-01-20 14:36:25 -0800

    04_03 solve

--- python-201-main/04_functions-and-scopes/04_03_write_letter.py
+++ python-201-main/04_functions-and-scopes/04_03_write_letter.py
@@ -3,3 +3,12 @@
 # message with the `name` input, as well as a goodbye message that uses
 # the `name` again. Combine that with the input `text` to return a
 # complete `letter`.
+
+
+def write_letter(name, text):
+    greeting = f"Hello, {name}! {text}"
+    goodbye = f"Goodbye, {name}! {text}"
+    return f"{greeting}\n{goodbye}"
+
+print(write_letter("Sean", "this is our letter text"))
+    
