76de49434cdb156593f2a92377bff759808531f7
Sean Burke <seanburke012@gmail.com>
2025-01-20 19:59:16 -0800

    04_06 solution

--- python-201-main/04_functions-and-scopes/04_06_sandwich_stand.py
+++ python-201-main/04_functions-and-scopes/04_06_sandwich_stand.py
@@ -3,3 +3,9 @@
 # - takes an arbitrary amount of toppings
 # - returns a string representing a sandwich with the bread on top
 #   and bottom, and the toppings in between.
+
+def make_sandwich(bread, *toppings):
+    topping_string = ", ".join(toppings)
+    return f"your sandwich consists of {bread}, {str(topping_string)}, {bread}"
+
+print(make_sandwich("sourdough", "peanut butter", "jelly"))
