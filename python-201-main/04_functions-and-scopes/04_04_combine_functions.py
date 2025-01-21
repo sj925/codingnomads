838b14d00d51a4265224215609732624b8c6ae24
Sean Burke <seanburke012@gmail.com>
2025-01-20 17:53:39 -0800

    04_04 solution

--- python-201-main/04_functions-and-scopes/04_04_combine_functions.py
+++ python-201-main/04_functions-and-scopes/04_04_combine_functions.py
@@ -2,3 +2,17 @@
 # with the `write_letter()` function from the previous exercise.
 # Write both functions in this script and call `greet()` within `write_letter()`
 # to let `greet()` take care of creating the greeting string.
+
+
+def greet(greeting, name):
+    sentence = f"{greeting}, {name}! How are you?"
+    return sentence
+
+
+def write_letter(greeting, name, text):
+    greeting_message = greet(greeting, name)
+    letter_body = f"{text}"
+    goodbye = f"Goodbye, {name}! Take care."
+    return f"{greeting_message}\n\n{letter_body}\n\n{goodbye}"
+
+print(write_letter("Hello", "Sean", "I hope this message finds you well."))
