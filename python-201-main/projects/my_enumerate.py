c925754abf125bb890cf34513df2758ca0a926bb
Sean Burke <seanburke012@gmail.com>
2025-01-22 20:12:59 -0800

    enumerator project

--- python-201-main/projects/my_enumerate.py
+++ python-201-main/projects/my_enumerate.py
@@ -2,6 +2,14 @@
 # Define a function called `my_enumerate()` that takes an iterable as input
 # and gives back both the element as well as its index position as an integer.
 
-def my_enumerate():  # add your arguments
-      # add your code implementation
-      pass
+def my_enumerate(iterable):
+      index = 0
+      for item in iterable:
+            yield index, item
+      index += 1
+      
+
+courses = ['Intro', 'Intermediate', 'Advanced', 'Professional']
+    
+for index, item in my_enumerate(courses):
+      print(f"{index}: {item}")
