d09371dc2b6a88f5d6f3d0a39e440289e6a3edfd
Sean Burke <seanburke012@gmail.com>
2025-01-22 19:11:32 -0800

    o4_11 solve

--- python-201-main/04_functions-and-scopes/04_11_your_realtor.py
+++ python-201-main/04_functions-and-scopes/04_11_your_realtor.py
@@ -3,3 +3,16 @@
 # is why your function should be able to take an arbitrary amount of
 # keyword arguments, and display them all in a list form with some 
 # introductory information.
+
+def advert(**kwargs: str) -> list[str]:
+    """takes in key/value args and returns a list of the information"""
+    print("Real Estate advertisement information:\n")
+    details_list = [f"{k.capitalize()}: {v}" for k,v in kwargs.items()]
+    details = ("\n".join(details_list))
+    return details
+
+print(advert(
+    home="a wonderful 4bd 3bath two story home",
+    price="$500,000",
+    agent="Sean Burke",
+    contact="555-555-0000"))
