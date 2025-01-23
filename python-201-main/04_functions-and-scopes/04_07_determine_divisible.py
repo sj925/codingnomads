283db8ff1233fa4cf94ce9148f3c585a5a9d9d3b
Sean Burke <seanburke012@gmail.com>
2025-01-22 18:14:15 -0800

    04_07 solution

--- python-201-main/04_functions-and-scopes/04_07_determine_divisible.py
+++ python-201-main/04_functions-and-scopes/04_07_determine_divisible.py
@@ -7,3 +7,30 @@
 # - call your functions, passing in the user input as the arguments,
 #   and set their output equal to new variables 
 # - print your the result variables with descriptive messages
+
+def divisible_by_four_or_seven(num):
+    if num % 4 == 0 or num % 7 == 0:
+        return True
+    return False
+    
+def divisible_by_four_and_seven(num):
+     if num % 4 == 0 and num % 7 == 0:
+        return True
+     return False
+
+try:
+    user_input = int(input("enter a number between 1 and 1,000,000,000: "))
+    if 1 <= user_input <= 1000000000:
+        print(f"is {user_input} divisible by four and seven? {divisible_by_four_and_seven(user_input)}")
+        print(f"is {user_input} divisible by four or seven? {divisible_by_four_or_seven(user_input)}")
+    else:
+        print("number out of defined range")
+except ValueError:
+    print("invalid input type")
+    
+
+    
+
+    
+
+    
