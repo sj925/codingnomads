008bed846028640287d59b89aca7f04f2218a28b
Sean Burke <seanburke012@gmail.com>
2025-01-22 18:38:03 -0800

    04_09 solve

--- python-201-main/04_functions-and-scopes/04_09_three_functions.py
+++ python-201-main/04_functions-and-scopes/04_09_three_functions.py
@@ -3,3 +3,21 @@
 # of that function to do something with it. You can have more
 # than three functions, and they don't need to call each other
 # in a circular way.
+
+def add(x, y):
+    return x+y
+
+def subtract(x, y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x, y):
+    return x / y
+
+my_num = add(5, subtract(5,4))
+another_num = subtract(27, multiply(4, 3))
+another_another_num = (divide(100, add(5,5)))
+
+print(my_num, another_another_num, another_num)
