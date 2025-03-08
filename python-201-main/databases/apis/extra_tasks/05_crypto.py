2993555d53e76ef73bf2d303d2b34dfdfe364ac4
Sean Burke <seanburke012@gmail.com>
2025-03-07 19:00:14 -0800

    adding exercises

---
+++ python-201-main/databases/apis/extra_tasks/05_crypto.py
@@ -0,0 +1,16 @@
+'''
+https://coinmarketcap.com/api/pricing/
+Use the CoinMarketCap API, to repeatedly fetch the price of Bitcoin for a duration of 10 minutes.
+Store each value in a dictionary that uses the time of query as a key.
+
+After the script stopped running, determine programmatically at what query time the price
+was the highest, and when the lowest.
+
+HINTS:
+- You can pick a different API from this list: https://github.com/public-apis/public-apis#cryptocurrency
+- You may have to request an API key first. If you need one, remember to include it in your queries.
+- You can use packages from the standard library, e.g. for time keeping and dates
+
+BONUS: Explore the `logging` package for easier tracking
+
+'''
