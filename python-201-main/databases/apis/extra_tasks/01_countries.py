1035b5de29aa2cf21ca8eb68526c833677aa7f75
Sean Burke <seanburke012@gmail.com>
2025-03-10 20:34:11 -0700

    01_countries

--- python-201-main/databases/apis/extra_tasks/01_countries.py
+++ python-201-main/databases/apis/extra_tasks/01_countries.py
@@ -8,3 +8,15 @@ In your python program, parse and compare the data of the two responses:
 * Print the native name of both countries, as well as their capitals
 
 '''
+import requests 
+from pprint import pprint 
+url = "https://restcountries.com/v3.1/name"
+country1 = "United States"
+country2 = "Ireland"
+response1 = requests.get(f"{url}/{country1}")
+response2 = requests.get(f"{url}/{country2}")
+usa_data = (response1.json()[1])
+ireland_data = (response2.json()[0])
+usa_population = usa_data["population"]
+ireland_population = ireland_data["population"]
+print(f"the US population is: {usa_population}, the Irish population is: {ireland_population}")
